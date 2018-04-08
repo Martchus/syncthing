13a8b58e40f384226bd1e95ff2425b2257b8f268
Martchus <martchus@gmx.net>
2018-04-08 20:01:43 +0200

    Add basic C bindings to start and stop Syncthing and invoke its CLI

--- cmd/syncthing/debug.go
+++ cmd/syncthing/debug.go
@@ -4,12 +4,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-package main
+package syncthing_main
 
 import (
 	"github.com/syncthing/syncthing/lib/logger"
 )
 
 var (
-	l = logger.DefaultLogger.NewFacility("main", "Main package")
+	l = logger.DefaultLogger.NewFacility("syncthing_main", "Syncthing package")
 )
