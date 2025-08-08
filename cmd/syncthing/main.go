047ce342866582b5517bb13df712e00f37b20e4e
Martchus <martchus@gmx.net>
2025-08-08 13:54:39 +0200

    Add basic C bindings to start and stop Syncthing and invoke its CLI

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-package main
+package syncthing_main
 
 import (
 	"bytes"
@@ -36,7 +36,6 @@ import (
 	"github.com/thejerf/suture/v4"
 	"github.com/willabides/kongplete"
 
-	"github.com/syncthing/syncthing/cmd/syncthing/cli"
 	"github.com/syncthing/syncthing/cmd/syncthing/decrypt"
 	"github.com/syncthing/syncthing/cmd/syncthing/generate"
 	"github.com/syncthing/syncthing/internal/db"
@@ -128,7 +127,6 @@ type CLI struct {
 	VersionFlag bool   `name:"version" help:"Show current version, then exit"`
 
 	Serve serveCmd `cmd:"" help:"Run Syncthing (default)" default:"withargs"`
-	CLI   cli.CLI  `cmd:"" help:"Command line interface for Syncthing"`
 
 	Browser  browserCmd   `cmd:"" help:"Open GUI in browser, then exit"`
 	Decrypt  decrypt.CLI  `cmd:"" help:"Decrypt or verify an encrypted folder"`
@@ -138,8 +136,6 @@ type CLI struct {
 	Upgrade  upgradeCmd   `cmd:"" help:"Perform or check for upgrade, then exit"`
 	Version  versionCmd   `cmd:"" help:"Show current version, then exit"`
 	Debug    debugCmd     `cmd:"" help:"Various debugging commands"`
-
-	InstallCompletions kongplete.InstallCompletions `cmd:"" help:"Print commands to install shell completions"`
 }
 
 func (c *CLI) AfterApply() error {
@@ -204,7 +200,7 @@ func defaultVars() kong.Vars {
 	return vars
 }
 
-func main() {
+func RunWithArgs(args []string) error {
 	// Create a parser with an overridden help function to print our extra
 	// help info.
 	var entrypoint CLI
@@ -223,16 +219,16 @@ func main() {
 	}
 
 	kongplete.Complete(parser)
-	ctx, err := parser.Parse(os.Args[1:])
+	ctx, err := parser.Parse(args)
 	parser.FatalIfErrorf(err)
 
 	if entrypoint.VersionFlag {
-		_ = versionCmd{}.Run()
-		return
+		return versionCmd{}.Run()
 	}
 
 	err = ctx.Run()
 	parser.FatalIfErrorf(err)
+	return err
 }
 
 func helpHandler(options kong.HelpOptions, ctx *kong.Context) error {
@@ -639,6 +635,10 @@ func loadOrDefaultConfig() (config.Wrapper, error) {
 	}
 
 	return cfg, err
+}
+
+func MakeAuditWriter(auditFile string) io.Writer {
+	return auditWriter(auditFile)
 }
 
 func auditWriter(auditFile string) io.Writer {
