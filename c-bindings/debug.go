a9d33b31fa2608b74dc4d8d3a0bf1e02ca2435af
Martchus <martchus@gmx.net>
2025-09-05 14:31:06 +0200

    Add basic C bindings to start and stop Syncthing and invoke its CLI

--- c-bindings/debug.go
+++ c-bindings/debug.go
@@ -1,16 +1,13 @@
-// Copyright (C) 2016 The Syncthing Authors.
+// Copyright (C) 2014 The Syncthing Authors.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//go:build go1.7
+package main
 
-package syncthing_main
+import (
+	"github.com/syncthing/syncthing/internal/slogutil"
+)
 
-import "runtime/debug"
-
-func init() {
-	// We want all (our) goroutines in panic traces.
-	debug.SetTraceback("all")
-}
+func init() { slogutil.RegisterPackage("Main package") }
--- cmd/syncthing/perfstats_unix.go
+++ cmd/syncthing/perfstats_unix.go
@@ -6,7 +6,7 @@
 
 //go:build !solaris && !windows
 
-package main
+package syncthing_main
 
 import (
 	"fmt"
